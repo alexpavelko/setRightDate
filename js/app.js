a1082ebe2cb1db37014fb029ec6494d96c2426c8
alexpavelko <pavelkobond@gmail.com>
2021-02-20 02:39:12 +0200

    Release. Sets right date

--- js/app.js
+++ js/app.js
@@ -10,6 +10,25 @@ function ParseUserData(){
     const UserMinutes = document.querySelector("#Minutes").value;
 
     const UserSeconds = document.querySelector("#Seconds").value;
+
+    createDate(UserDay,UserMonth,UserYear,UserHours,UserMinutes,UserSeconds);
+}
+
+function createDate(UserDay,UserMonth,UserYear,UserHours,UserMinutes,UserSeconds){
+  
+        console.clear();
+ 
+        UserMonth-=1;
+
+        let lastDay = GetLastDayOfMonth(UserYear,UserMonth); 
+
+       
+        if(UserDay > lastDay)
+        UserDay = lastDay;
+
+        const anyDate = new Date(UserYear,UserMonth,UserDay,UserHours,UserMinutes,UserSeconds); 
+  
+        console.log(anyDate.toLocaleString()); 
 }
 
 function GetLastDayOfMonth(year,month){
