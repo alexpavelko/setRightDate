33ac8bba1f5d2c586a3141f80ca4ba5f235b2c8e
alexpavelko <pavelkobond@gmail.com>
2021-02-20 02:29:53 +0200

    parsing user data

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function ParseUserData(){
+    var UserDay = document.querySelector("#Day").value;
+
+    var UserMonth = document.querySelector("#Month").value;
+
+    const UserYear = document.querySelector("#Year").value;
+
+    const UserHours = document.querySelector("#Hours").value;
+    
+    const UserMinutes = document.querySelector("#Minutes").value;
+
+    const UserSeconds = document.querySelector("#Seconds").value;
+}
